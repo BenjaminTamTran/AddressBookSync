39d57d74c5891f40152de808e9a6495b760c4b4b
BenjaminTamTran <benjamintamtran@gmail.com>
2015-10-03 00:34:07 +0700

    Save to User Default and check

---
+++ AddressBookSync/Config.swift
@@ -0,0 +1,11 @@
+//
+//  Config.swift
+//  AddressBookSync
+//
+//  Created by Tran Huu Tam on 10/2/15.
+//  Copyright © 2015 Benjaminsoft. All rights reserved.
+//
+
+import Foundation
+
+let kAddressBookDataKey = "AddressBookDataKey"
--- AddressBookSync/ViewController.swift
+++ AddressBookSync/ViewController.swift
@@ -79,13 +79,17 @@ class ViewController: UIViewController {
     
     private func syncAddressBook() {
         let addressBookRef: ABAddressBook? = ABAddressBookCreateWithOptions(nil, nil).takeRetainedValue()
+        var currentContactsList = [Dictionary<String, String>]()
         if let _ = addressBookRef {
+            var contactDetails = [String : String]()
             self.statusLabel.text = "Syncing is in progress. Please wait."
             self.syncButton.hidden = true
             let allContacts = ABAddressBookCopyArrayOfAllPeople(addressBookRef).takeRetainedValue() as Array
             for record in allContacts {
                 let contact: ABRecordRef = record
                 var contactFullName = ""
+                var contactPhoneNumber = ""
+                let contactid = ABRecordGetRecordID(contact)
                 if let _ = ABRecordCopyValue(contact, kABPersonFirstNameProperty), let _ = ABRecordCopyValue(contact, kABPersonLastNameProperty) {
                     let firstName = ABRecordCopyValue(contact, kABPersonFirstNameProperty).takeRetainedValue() as! String
                     let lastName = ABRecordCopyValue(contact, kABPersonLastNameProperty).takeRetainedValue() as! String
@@ -99,18 +103,49 @@ class ViewController: UIViewController {
                     let lastName = ABRecordCopyValue(contact, kABPersonLastNameProperty).takeRetainedValue() as! String
                     contactFullName = lastName
                 }
-                print(contactFullName)
                 let contactPhoneNumbers = ABRecordCopyValue(contact, kABPersonPhoneProperty).takeRetainedValue()
                 if ABMultiValueGetCount(contactPhoneNumbers) > 0 {
-                    let contactPhoneNumber = ABMultiValueCopyValueAtIndex(contactPhoneNumbers, 0).takeRetainedValue() as! String
-                    print(contactPhoneNumber)
+                    contactPhoneNumber = ABMultiValueCopyValueAtIndex(contactPhoneNumbers, 0).takeRetainedValue() as! String
+                    
                 }
+                contactDetails["ID"] = "\(contactid)"
+                contactDetails["Name"] = contactFullName
+                contactDetails["Phone"] = contactPhoneNumber
+                currentContactsList.append(contactDetails)
             }
         }
-    }
-    
-    private func registerListenAddressBookChange() {
-        
+//        print(currentContactsList)
+        let def = NSUserDefaults.standardUserDefaults()
+        if let savedContacts = def.objectForKey(kAddressBookDataKey) {
+            let savedContactsArray = savedContacts as! [Dictionary<String, String>]
+            var isChanged = false
+            for contact in savedContactsArray {
+                for contactCompare in currentContactsList {
+                    if contact["ID"] == contactCompare["ID"] {
+                        let name = contact["Name"]
+                        if name == contactCompare["Name"] {
+                            if contact["Phone"] != contactCompare["Phone"] {
+                                isChanged = true
+                                let phone = contactCompare["Phone"]
+                                print("Difference Phone in \(phone)")
+                            }
+                        }
+                        else {
+                            isChanged = true
+                            print("Difference Name in \(name)")
+                        }
+                    }
+                }
+            }
+            self.syncButton.hidden = false
+            if !isChanged {
+//                self.statusLabel.text = "Syncing is in progress. Please wait."
+            }
+        }
+        else {
+            def.setObject(currentContactsList, forKey: kAddressBookDataKey)
+            def.synchronize()
+        }
     }
 }
 
